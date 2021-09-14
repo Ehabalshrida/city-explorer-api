c4b92fe765590406b0574c1dc3df209585e80183
ehabalshrida <ehabshridah@gmail.com>
2021-09-14 20:13:04 +0300

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express'); 
 const server = express();
 const cors = require('cors');
+const axios=require("axios");
 server.use(cors());
 require('dotenv').config();
 const weatherData = require('./data/weather.json');
@@ -10,9 +11,63 @@ const PORT = process.env.PORT;
 
 
 server.get('/test', (req, res) => {
-    res.status(200).send('hello world')
+    res.status(200).send('hello world')});
 
-})
+    let handleMovie= async (req,res)=>{
+        let city = req.query.searchQuery;
+        let url=`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+        let axiosResponsee= await axios.get(url);
+        let movieResponse=axiosResponsee.data;
+        let cleanedDataa=movieResponse.data.map(item=>{
+            return new Movie(element);
+        })
+        res.status(200).json(cleanedDataa);
+    }
+
+
+
+
+    server.get('/movie', handleMovie);
+    
+    class Movie {
+        constructor(item) {
+            this.title = item.original_title;
+            this.overview = item.overview;
+            this.avgVotes = item.vote_average;
+            this.totalVotes= item.vote_count;
+            this.popularity = item.popularity;
+            this.releaseDate = item.release_date;
+    
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+let handleWeather= async (req,res)=>{
+    let lat= Number(req.query.lat);
+    let lon= Number(req.query.lon)
+    let url=`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+    let axiosResponse= await axios.get(url);
+    let sweatresponse=axiosResponse.data;
+    let cleanedData=sweatresponse.data.map(item=>{
+        return new ForeCastarr(item.datetime,item.weather.description);
+    })
+    res.status(200).json(cleanedData);
+}
+server.get('/sweat',handleWeather)
+class ForeCastarr{
+    constructor(date,description){
+        this.date=date;
+        this.description=description
+    }
+}
 
 server.get('/weather',(req,res)=>{
     let lat=Number(req.query.lat);
